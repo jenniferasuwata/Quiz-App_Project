4ccf0be6e2586bf56e0f994b60dfd6f7e4445bae
BabaGee01 <jide_oloko@hotmail.co.uk>
2024-12-07 14:27:56 +0000

    terraform commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+# HCL to Create VPC, EKS Cluster  & Public Subnet
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+  }
+resource "aws_subnet" "public_subnet" {
+    count                   = 2
+    vpc_id                  = aws_vpc.main.id
+    cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+    map_public_ip_on_launch = true
+  }
+
+resource "aws_eks_cluster" "eks_cluster" {
+    name     = "quiz-app-cluster"
+    role_arn = aws_iam_role.eks_role.arn
+    vpc_config {
+      subnet_ids = aws_subnet.public_subnet[*].id
+    }
+  }
+  # HCL to Create IAM Role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+# Attach necessary policies to the EKS Role
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
